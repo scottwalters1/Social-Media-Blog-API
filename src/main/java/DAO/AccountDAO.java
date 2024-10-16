6e63fbe0ea3130e72b38b9407902210e0d312aa0
Scott Walters <scottw567@gmail.com>
2024-10-16 14:42:01 +0000

    Story 1: AccountDAO created

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,29 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+    
+    public Account insertAccount(Account account){
+        Connection conn = ConnectionUtil.getConnection();
+
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, account.getUsername());
+            statement.setString(2, account.getPassword());
+
+            statement.executeUpdate();
+            return account;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
